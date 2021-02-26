9998fab4e0441414258160803a6e569d4bed1552
eivankin <>
2021-02-26 22:16:13 +0300

    added api for model User

---
+++ data/users_api.py
@@ -0,0 +1,68 @@
+from flask import jsonify, Blueprint, abort, request
+from . import db_session
+from .models import User
+
+users_attrs = ('id', 'surname', 'name', 'age', 'position', 'speciality',
+               'address', 'email', 'modified_date')
+users_fields = ('surname', 'name', 'age', 'position', 'speciality',
+                'address', 'email', 'modified_date', 'password')
+db_session.global_init('db/mars_explorer.sqlite')
+session = db_session.create_session()
+blueprint = Blueprint(
+    'users_api',
+    __name__,
+    template_folder='templates'
+)
+
+
+@blueprint.route('/api/users', methods=['GET'])
+def get_users():
+    return jsonify({'jobs': [user.to_dict(only=users_attrs)
+                             for user in session.query(User).all()]})
+
+
+@blueprint.route('/api/users/<int:user_id>', methods=['GET'])
+def get_one_user(user_id):
+    user = session.query(User).get(user_id)
+    if not user:
+        abort(404)
+    return jsonify(user.to_dict(only=users_attrs))
+
+
+@blueprint.route('/api/users', methods=['POST'])
+def create_user():
+    if not request.json or any(key not in request.json for key in users_fields):
+        abort(400)
+    tmp = request.json.copy()
+    del tmp['password']
+    user = User(**tmp)
+    user.set_password(request.json['password'])
+    session.add(user)
+    session.commit()
+    return jsonify({'status': 'ok'})
+
+
+@blueprint.route('/api/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = session.query(User).get(user_id)
+    if not user:
+        abort(404)
+    session.delete(user)
+    session.commit()
+    return jsonify({'status': 'ok'})
+
+
+@blueprint.route('/api/users/<int:user_id>', methods=['PUT'])
+def edit_user(user_id):
+    user = session.query(User).get(user_id)
+    if not user:
+        abort(404)
+    if not request.json or any(key not in request.json for key in users_fields):
+        abort(400)
+    tmp = request.json.copy()
+    del tmp['password']
+    for key, value in tmp.items():
+        setattr(user, key, value)
+    user.set_password(request.json['password'])
+    session.commit()
+    return jsonify({'status': 'ok'})
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, redirect, request, make_response, json
 from dotenv import load_dotenv
 from flask_login import LoginManager, login_user, login_required, \
     logout_user, current_user
-from data import db_session, jobs_api
+from data import db_session, jobs_api, users_api
 from data.models import User, Jobs, Department, Category
 from forms import RegisterForm, LoginForm, JobForm, DepartmentForm
 
@@ -211,4 +211,5 @@ if __name__ == '__main__':
     db_session.global_init('db/mars_explorer.sqlite')
     session = db_session.create_session()
     app.register_blueprint(jobs_api.blueprint)
+    app.register_blueprint(users_api.blueprint)
     app.run(port=8080, host='127.0.0.1')
